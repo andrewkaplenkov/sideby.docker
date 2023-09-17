8134f3e27d42317047109ba218792bd85422a3e5
Andrew Kaplenkov <kaplenkov.andrew@gmail.com>
2023-09-17 19:05:12 +0500

    reload init

--- public/index.php
+++ public/index.php
@@ -1,21 +1,3 @@
 <?php
 
-use Lilo\Core\App;
-use Lilo\Core\Http\Kernel;
-use Lilo\Core\Http\Request;
-
-
-define('BASE_PATH', dirname(__DIR__));
-include BASE_PATH . '/core/helpers.php';
-
-require_once BASE_PATH . '/vendor/autoload.php';
-
-App::create();
-
-$factory = App::resolve(\DB\Factories\UserFactory::class);
-dd($factory->create_model);
-
-(new Kernel())
-    ->handle(App::resolve(Request::class))
-    ->send();
-
+phpinfo();
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\UserController;
-use Lilo\Core\Router\Route;
-
-return [
-    Route::get('/users', [UserController::class, 'index']),
-    Route::get('/users/{id:\d+}', [UserController::class, 'show'])
-];
